86d1d97b337380e15436d095bc42772f2c89f255
Szilard Kovacs <kovszilard@gmail.com>
2019-09-18 15:10:08 +0200

    added changeState function

--- src/main/scala/menu/Menu.scala
+++ src/main/scala/menu/Menu.scala
@@ -1,5 +1,7 @@
 package menu
 
+import menu.KeyPress._
+
 object Menus {
 
   case class SingleChoiceMenu(title: String, options: List[String], selected: Int = 0)
@@ -12,13 +14,17 @@ object Menus {
 
     s"${m.title}\n${options.mkString("\n")}"
   }
+
+  def changeState(input: KeyPress, m: SingleChoiceMenu): SingleChoiceMenu = input match {
+    case Up if m.selected > 0 => m.copy(selected = m.selected - 1)
+    case Down if m.selected < m.options.size - 1 => m.copy(selected = m.selected + 1)
+    case _ => m
+  }
 }
 
-object Test extends App {
+trait KeyPress
+object KeyPress {
 
-  import Menus._
-
-  val menu = SingleChoiceMenu("What is your favourite fruit?", List("Apple", "Banana"))
-
-  println(menuString(menu))
+  case object Up extends KeyPress
+  case object Down extends KeyPress
 }
--- src/test/scala/example/MenuTest.scala
+++ src/test/scala/example/MenuTest.scala
@@ -1,11 +1,12 @@
 package example
 
+import menu.KeyPress._
 import menu.Menus._
 import org.scalatest._
 
 class MenuTest extends FlatSpec with Matchers {
 
-  "A SingleChoiceMenu" should "have a well formed string representation" in {
+  "A SingleChoiceMenu" should "have a well formatted string representation" in {
 
     val menu1 = SingleChoiceMenu("What is your favourite fruit?", List("Apple", "Banana"))
 
@@ -23,6 +24,20 @@ class MenuTest extends FlatSpec with Matchers {
 
     menuString(menu1) shouldEqual expected1
     menuString(menu2) shouldEqual expected2
+  }
+
+  "changeState" should "handle Up and Down keys" in {
+
+    val s0 = SingleChoiceMenu("Title", List("Option1", "Option2"), selected = 0)
+
+    val s1 = changeState(Up, s0)
+    s1.selected shouldEqual 0
+
+    val s2 = changeState(Down, s1)
+    s2.selected shouldEqual 1
+
+    val s3 = changeState(Down, s2)
+    s3.selected shouldEqual 1
   }
 
 }
