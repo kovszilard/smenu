824dc92c2408b0a0a53a13bdefa7adab4f1e24b5
Szilard Kovacs <kovszilard@gmail.com>
2019-09-18 14:03:49 +0200

    added more tests

--- src/test/scala/example/MenuTest.scala
+++ src/test/scala/example/MenuTest.scala
@@ -7,13 +7,22 @@ class MenuTest extends FlatSpec with Matchers {
 
   "A SingleChoiceMenu" should "have a well formed string representation" in {
 
-    val menu = SingleChoiceMenu("What is your favourite fruit?", List("Apple", "Banana"))
+    val menu1 = SingleChoiceMenu("What is your favourite fruit?", List("Apple", "Banana"))
 
-    val expected =
+    val expected1 =
       """|What is your favourite fruit?
          |> Apple
          |  Banana""".stripMargin
 
-    menuString(menu) shouldEqual expected
+    val menu2 = SingleChoiceMenu("What is your favourite fruit?", List("Apple", "Banana"), selected = 1)
+
+    val expected2 =
+      """|What is your favourite fruit?
+         |  Apple
+         |> Banana""".stripMargin
+
+    menuString(menu1) shouldEqual expected1
+    menuString(menu2) shouldEqual expected2
   }
+
 }
