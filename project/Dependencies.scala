8f0d3c9fc1a652f494a3fb453f3082636d70582b
Scala Steward <me@scala-steward.org>
2019-12-02 06:38:36 +0100

    Update scalatest to 3.1.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -1,7 +1,7 @@
 import sbt._
 
 object Dependencies {
-  lazy val scalaTest = "org.scalatest" %% "scalatest" % "3.0.8"
+  lazy val scalaTest = "org.scalatest" %% "scalatest" % "3.1.0"
   lazy val cats = "org.typelevel" %% "cats-core" % "2.0.0"
   lazy val catsEffect = "org.typelevel" %% "cats-effect" % "2.0.0"
   lazy val jline = "org.jline" % "jline" % "3.13.0"
