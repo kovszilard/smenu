c27c829169920236bd9f7b6c751dd75098f3be74
Scala Steward <me@scala-steward.org>
2020-01-07 16:22:49 +0100

    Update jline to 3.13.3

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -4,5 +4,5 @@ object Dependencies {
   lazy val scalaTest = "org.scalatest" %% "scalatest" % "3.0.8"
   lazy val cats = "org.typelevel" %% "cats-core" % "2.0.0"
   lazy val catsEffect = "org.typelevel" %% "cats-effect" % "2.0.0"
-  lazy val jline = "org.jline" % "jline" % "3.13.0"
+  lazy val jline = "org.jline" % "jline" % "3.13.3"
 }
